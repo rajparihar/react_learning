c864743cb4bcca7eddf82e77560e0bab6dc8dbee
rajkumarj6@gmail.com <rajkumarj6@gmail.com>
2018-07-05 18:47:13 +0530

    Worked on style,event,dynamic component display.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react';
 import './App.css';
-import Person from './Person/Person'
+
+
+
+// import Person from './Person/Person'
+import PersonCard from './PersonCard/PersonCard';
 
 class App extends Component {
   
   state={
     person:[
-      {name:"Mike",age:"28"},
-      {name:"Harvey",age:"30"}
-    ] 
+      {id:"1",name:"Mike",age:"28",email:"mike@gmail.com"},
+      {id:"2",name:"Harvey",age:"30",email:"harvey@gmail.com"},
+      {id:"3",name:"Rachel",age:"21",email:"rachel@gmail.com"},
+      {id:"4",name:"Max",age:"35",email:"max@gmail.com"}
+    ],
+    show:false
   }
 
   changeNameHandler=(name,age)=>{
@@ -19,20 +26,95 @@ class App extends Component {
       ]
     })
   }
+  showRecordshandler=()=>{
+    let dowsShow=this.state.show;
+    this.setState({show: !dowsShow })
+  }
+  
+
+  nameChangehandler=(event,personID)=>{
+    //findIndex will return the index of matched person
+    const empIndex=this.state.person.findIndex(p=>{return p.id === personID;});
+    const emp={
+      ...this.state.person[empIndex]  
+    }
+
+    emp.name=event.target.value;
+
+    const persons=[...this.state.person];
+    persons[empIndex]=emp;
+    
+    this.setState({
+      person:persons
+    })
 
 
+    
+  }
+
+  deleteHandlerHandler= personIndex=>{
+    // let allPersonData = this.state.person.slice();
+    let allPersonData = [...this.state.person];
+    allPersonData.splice(personIndex,1);
+    this.setState({person:allPersonData});
+  }
   
 
   render() {
+
+    let btnShowHide={
+      backgroundColor:"white",
+      font:"inherit",
+      border:"1px solid blue",
+      padding:"8px",
+      cursor:"pointer",
+      marginBottom:"20px",
+      marginRight:"20px"
+    };
+
+    let  person=null;
+
+    if(this.state.show){
+      person=(
+      <div>
+      {
+        this.state.person.map((ele,i)=>{
+          return (<PersonCard 
+             name={ele.name} 
+             age={ele.age}  
+             email={ele.email} 
+             deleteHandler={()=>this.deleteHandlerHandler(i)}
+             key={ele.id}
+             change={(event)=>this.nameChangehandler(event,ele.id)}
+             />);
+        })
+      }
+      </div>
+      );
+    }
+
     return (
       <div className="App">
-        <button onClick={this.changeNameHandler.bind(this,'raj','24')}>Click Me!</button>
-        <Person click={this.changeNameHandler} age={this.state.person[0].age} name={this.state.person[0].name} />
-        <Person/>
-        <Person /> 
+        <h1> Employees Detail</h1>
+        <button style={btnShowHide} onClick={this.showRecordshandler}>Show Employees </button>
+        <button style={btnShowHide} onClick={this.addEmployeeHandler}>Add Employee</button>
+        <div className="container">
+          {person}
+        </div>
       </div>
     );
   }
 }
 
 export default App;
+
+
+//pass fucntion as handler
+// <button onClick={this.changeNameHandler.bind(this,'raj','24')}>Click Me!</button>
+//         <Person click={this.changeNameHandler} age={this.state.person[0].age} name={this.state.person[0].name} />
+//         <Person/>
+//        <Person /> 
+// <PersonCard  name={this.state.person[0].name} age={this.state.person[0].age}  email={this.state.person[0].email}/>
+//         <PersonCard  name={this.state.person[1].name} age={this.state.person[1].age}  email={this.state.person[1].email}/>
+//         <PersonCard  name={this.state.person[2].name} age={this.state.person[2].age}  email={this.state.person[1].email}/>
+//         <PersonCard  name={this.state.person[3].name} age={this.state.person[3].age}  email={this.state.person[1].email}/>
---
+++ src/PersonCard/PersonCard.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import '../App.css';
+
+
+
+const PersonCard=(props)=>{
+    return (
+        <div className="box">
+            <h2 onClick={props.deleteHandler}  title="click me to delete!">Name: {props.name}</h2>
+            <h3 >Age: {props.age}</h3>
+            <h3>Email: {props.email} </h3>
+           <input type="text" onChange={props.change} value={props.name}/>
+        </div>
+    );
+}
+
+
+export default PersonCard;
